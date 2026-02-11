e30cb5309ae1c5528a02d7b0d2610240a8626d40
Jonathan <jccroxton@gmail.com>
2026-02-10 22:34:35 -0500

    test(gui): Disable pop-up windows in offscreen mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,7 @@
 """Shared test fixtures."""
+
+import os
+
+# Use Qt's offscreen platform plugin so GUI tests never create visible windows.
+# This must be set before any PyQt6 import.
+os.environ.setdefault("QT_QPA_PLATFORM", "offscreen")
